de0d77376ac7ba3b5f12b7047a88f81947f2c0fc
Blaz Vrabec <bvrabec-sola@proton.me>
2026-01-13 10:17:37 +0100

    bonus

---
+++ PROGRAMIRANJE1/VajaZaBonus/bonus.cs
@@ -0,0 +1,13 @@
+namespace VajaZaBonus
+{
+    internal class bonus
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Na kmetiji je lepo");
+            Console.WriteLine("I-A-I-A-O");
+            Console.WriteLine("Tam imamo osla dva");
+            Console.WriteLine("I-A-I-A-O");
+        }
+    }
+}
